4da5410612176e0189a6a0dace5853de6e21086e
Dragan Milisav <dmilisav@fb.com>
2013-04-13 12:46:16 -0700

    Database files ready to use

---
+++ AutismApplication/src/com/db/DBHelper.java
@@ -0,0 +1,36 @@
+package com.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Should one per table
+ */
+public class DBHelper extends SQLiteOpenHelper {
+
+	//this is our global database config values
+	private static final String DATABASE_NAME = "Autism.db";
+	private static final int DATABASE_VERSION = 1;
+	
+	//this is used by all tables
+	public static final String PK_ID_COL_NAME = "_id";
+	
+	public DBHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase database) {
+		//create all the tables in the db 
+		database.execSQL(TaskData.CREATE);
+		database.execSQL(ReminderData.CREATE);
+		database.execSQL(PhotoData.CREATE);
+		database.execSQL(NoteData.CREATE);
+		database.execSQL(ContactData.CREATE);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
+
+}
---
+++ AutismApplication/src/com/db/NoteData.java
@@ -0,0 +1,63 @@
+package com.db;
+
+public class NoteData implements Data {
+	public static final String TABLE_NAME = "notes";
+	public static final String FK = "task_id";
+	public static final String NAME = "name";
+	public static final String DESC = "desc";
+	public static final String CREATE = "create table if not exists "
+			+ TABLE_NAME + "(" + DBHelper.PK_ID_COL_NAME
+			+ " integer primary key autoincrement, " + FK
+			+ " integer not null," + NAME + " text not null," + DESC
+			+ " text not null" + ");";
+
+	public static final String[] allColumns = { DBHelper.PK_ID_COL_NAME, FK,
+			NAME, DESC };
+
+	private long id;
+	private long fk_id;
+	private String name;
+	private String desc;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	@Override
+	public String getTableName() {
+		return TABLE_NAME;
+	}
+
+	public long getFkId() {
+		return fk_id;
+	}
+
+	public void setFkId(long fk_id) {
+		this.fk_id = fk_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+}
